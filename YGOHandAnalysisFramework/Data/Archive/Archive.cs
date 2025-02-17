d869b6a841fdee6580e36e2bef52be637cde1e23
Matthew Livernoche <mattalivernoche@gmail.com>
2025-02-17 00:11:50 -0800

    First start of adding archive support

---
+++ YGOHandAnalysisFramework/Data/Archive/Archive.cs
@@ -0,0 +1,27 @@
+﻿using SharpCompress.Common;
+using SharpCompress.Writers;
+using System.Text.Json;
+using YGOHandAnalysisFramework.Data.Json;
+
+namespace YGOHandAnalysisFramework.Data.Archive;
+
+public static class Archive
+{
+    public static async Task WriteDecksToArchive(Stream destination, IEnumerable<DeckDTO> decks)
+    {
+        using var writer = WriterFactory.Open(destination, ArchiveType.Tar, new WriterOptions(CompressionType.None));
+
+        foreach(var deckDTO in decks)
+        {
+            var fileName = $"{deckDTO.Name}.json";
+
+            await using var deckStream = new MemoryStream();
+            await JsonSerializer.SerializeAsync(deckStream, deckDTO);
+
+            // reset position so writer can use it.
+            deckStream.Position = 0;
+
+            writer.Write(fileName, deckStream);
+        }
+    }
+}
