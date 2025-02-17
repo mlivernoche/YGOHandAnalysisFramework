9a54ae1e17b385eaff98d94f6d0cd1f3ba931fa6
Matthew Livernoche <mattalivernoche@gmail.com>
2025-02-17 00:10:30 -0800

    Add DeckDTO

---
+++ YGOHandAnalysisFramework/Data/Json/DeckDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace YGOHandAnalysisFramework.Data.Json;
+
+public sealed class DeckDTO
+{
+    public string Name { get; set; } = string.Empty;
+    public CardListDTO CardList { get; set; } = new CardListDTO();
+}
