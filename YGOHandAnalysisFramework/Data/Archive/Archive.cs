ef248f30c28683fdd15aa5fb12afa94f8a6b4f1d
Matthew Livernoche <mattalivernoche@gmail.com>
2025-02-19 20:17:10 -0800

    Add ability to read archives

--- YGOHandAnalysisFramework/Data/Archive/Archive.cs
+++ YGOHandAnalysisFramework/Data/Archive/Archive.cs
@@ -1,5 +1,8 @@
-﻿using SharpCompress.Common;
+﻿using CommunityToolkit.Diagnostics;
+using SharpCompress.Common;
+using SharpCompress.Readers;
 using SharpCompress.Writers;
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
 using YGOHandAnalysisFramework.Data.Json;
 
@@ -7,6 +10,46 @@ namespace YGOHandAnalysisFramework.Data.Archive;
 
 public static class Archive
 {
+    public static bool TryOpenArchive(Stream source, [NotNullWhen(true)] out IReader? reader)
+    {
+        reader = null;
+
+        try
+        {
+            reader = ReaderFactory.Open(source);
+            return true;
+        }
+        catch (InvalidOperationException)
+        {
+            return false;
+        }
+    }
+
+    public static async IAsyncEnumerable<DeckDTO> ReadDecksFromArchive(Stream source)
+    {
+        if(!TryOpenArchive(source, out var reader))
+        {
+            yield break;
+        }
+
+        using (reader)
+        {
+            while (reader.MoveToNextEntry())
+            {
+                if (reader.Entry.IsDirectory)
+                {
+                    continue;
+                }
+
+                await using var entryStream = reader.OpenEntryStream();
+                var deckDTO = await JsonSerializer.DeserializeAsync<DeckDTO>(entryStream);
+                Guard.IsNotNull(deckDTO, nameof(deckDTO));
+
+                yield return deckDTO;
+            }
+        }
+    }
+
     public static async Task WriteDecksToArchive(Stream destination, IEnumerable<DeckDTO> decks)
     {
         using var writer = WriterFactory.Open(destination, ArchiveType.Tar, new WriterOptions(CompressionType.None));
