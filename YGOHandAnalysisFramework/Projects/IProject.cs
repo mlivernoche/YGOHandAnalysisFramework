db19a76b498c5ebfb9707e1b70864196995c596b
Matthew Livernoche <mattalivernoche@gmail.com>
2025-03-01 10:17:48 -0800

    Add IProject<TCardGroupName> interface.

--- YGOHandAnalysisFramework/Projects/IProject.cs
+++ YGOHandAnalysisFramework/Projects/IProject.cs
@@ -5,3 +5,9 @@ public interface IProject
     string ProjectName { get; }
     void Run(IHandAnalyzerOutputStream outputStream);
 }
+
+public interface IProject<TCardGroupName> : IProject
+    where TCardGroupName : notnull, IEquatable<TCardGroupName>, IComparable<TCardGroupName>
+{
+    IEnumerable<TCardGroupName> SupportedCards { get; }
+}
