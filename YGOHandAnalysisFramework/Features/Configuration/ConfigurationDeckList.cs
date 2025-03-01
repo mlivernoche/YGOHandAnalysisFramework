e3bf5dd0c251164173a2095f9ff8599c18ee3084
Matthew Livernoche <mattalivernoche@gmail.com>
2025-02-28 20:37:45 -0800

    Put in seperate files

--- YGOHandAnalysisFramework/Features/Configuration/ConfigurationDeckList.cs
+++ YGOHandAnalysisFramework/Features/Configuration/ConfigurationDeckList.cs
@@ -2,9 +2,7 @@
 
 namespace YGOHandAnalysisFramework.Features.Configuration;
 
-public interface IConfigurationDeckList<TCardGroupName>
+public sealed record ConfigurationDeckList<TCardGroupName>(string Name, ICardGroupCollection<CardGroup<TCardGroupName>, TCardGroupName> Cards) : IConfigurationDeckList<TCardGroupName>
     where TCardGroupName : notnull, IEquatable<TCardGroupName>, IComparable<TCardGroupName>
 {
-    string Name { get; }
-    ICardGroupCollection<CardGroup<TCardGroupName>, TCardGroupName> Cards { get; }
 }
