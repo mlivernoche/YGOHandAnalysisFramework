4c9f80b2e32bf583d7a4a860d86f2bd03dd2dcd2
Matthew Livernoche <mattalivernoche@gmail.com>
2025-03-03 09:17:48 -0800

    Add abstract class for setting up common console setup scenarios.

---
+++ YGOHandAnalysisFramework/Features/Console/ConsoleAnalyzerComponents.cs
@@ -0,0 +1,25 @@
+﻿using YGOHandAnalysisFramework.Data;
+using YGOHandAnalysisFramework.Features.Configuration;
+using YGOHandAnalysisFramework.Projects;
+
+namespace YGOHandAnalysisFramework.Features.Console;
+
+public abstract class ConsoleAnalyzerComponents<TCardGroup, TCardGroupName>
+    where TCardGroup : ICardGroup<TCardGroupName>
+    where TCardGroupName : notnull, IEquatable<TCardGroupName>, IComparable<TCardGroupName>
+{
+    public abstract IEnumerable<IProject<TCardGroup, TCardGroupName>> CreateProjects();
+    public abstract TCardGroupName CreateCardGroupName(string name);
+    public abstract TCardGroup CreateMiscCardGroup(int size);
+    public abstract TCardGroup CreateCardGroup(ICardGroup<TCardGroupName> cardGroup);
+
+    public virtual IEnumerable<TCardGroupName> GetSupportedCards(IConfiguration<TCardGroupName> configuration)
+    {
+        return [];
+    }
+
+    public virtual IProjectHandler<TCardGroup, TCardGroupName> CreateProjectHandler()
+    {
+        return new ProjectHandler<TCardGroup, TCardGroupName>();
+    }
+}
