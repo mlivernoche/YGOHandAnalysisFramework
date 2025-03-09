938c8873350ac33fb7f5f8e8cb27bab935137f57
Matthew Livernoche <mattalivernoche@gmail.com>
2025-03-09 08:43:08 -0700

    Provide key with value.

--- YGOHandAnalysisFramework/Data/Extensions/Linq/DictionaryExtensions.cs
+++ YGOHandAnalysisFramework/Data/Extensions/Linq/DictionaryExtensions.cs
@@ -2,13 +2,13 @@
 
 public static class DictionaryExtensions
 {
-    public static IEnumerable<TValue> OrderBy<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dictionary, IEnumerable<TKey> order)
+    public static IEnumerable<KeyValuePair<TKey, TValue>> OrderBy<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dictionary, IEnumerable<TKey> order)
     {
         foreach (var key in order)
         {
             if (dictionary.TryGetValue(key, out var value))
             {
-                yield return value;
+                yield return new(key, value);
             }
         }
     }
