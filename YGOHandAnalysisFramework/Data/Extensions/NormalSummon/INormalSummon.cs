ef26bf9136219d6d713b35eafb72f2c12187a6c6
Matthew Livernoche <mattalivernoche@gmail.com>
2025-03-27 20:14:36 -0700

    Initial INormalSummon commit

---
+++ YGOHandAnalysisFramework/Data/Extensions/NormalSummon/INormalSummon.cs
@@ -0,0 +1,12 @@
+﻿namespace YGOHandAnalysisFramework.Data.Extensions.NormalSummon;
+
+public interface INormalSummon<TCardGroupName> : INamedCard<TCardGroupName>
+    where TCardGroupName : notnull, IEquatable<TCardGroupName>, IComparable<TCardGroupName>
+{
+    /// <summary>
+    /// This does not describe whether or not a card can be normal summoned. Rather, it is
+    /// talking about cards like Lava Golem or Sphere Mode, where normal summon is the card's
+    /// only use. Something like Ext Ryzeal would require a specific implementation.
+    /// </summary>
+    bool RequiresNormalSummon { get; }
+}
